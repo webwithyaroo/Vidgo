63e8ee0d4a422ac1bfa0089c2cc6fafd435dde18
Issa Soliu Ridollahi <olawaleridollahi@gmail.com>
2024-07-26 06:09:41 -0700

    Repo dir

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/reactapp/",
+  base: "/Vidgo/",
   plugins: [react()],
   build: {
+    outDir: "dist",
     chunkSizeWarningLimit: 4096, // Set the limit to 1024 KB or any desired value
   },
 });
